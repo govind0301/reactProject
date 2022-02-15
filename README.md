58a76fa9e27626942b4065f3f68604d5fe8e652a
govind0301 <govindsaxena03@gmail.com>
2022-02-15 18:18:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-nvuasx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-nvuasx)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const App = () => {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
+  );
+};
+
+export default App;
